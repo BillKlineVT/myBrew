9a05732b66fc50f12dd78a984cb4ce8e92aed06c
Bill Klinefelter <bill.klinefelter@gmail.com>
2019-10-13 15:53:37 -0500

    Adding GUITest using SikuliX project with Maven deps

---
+++ myBrewTests/GUITest/GUITest/src/main/java/com/billklinefelter/myBrewTests/GUITest/myBrewGUITest.java
@@ -0,0 +1,30 @@
+package com.billklinefelter.myBrewTests.GUITest;
+import org.sikuli.script.*;
+import static org.junit.Assert.*;
+import org.sikuli.script.ImagePath;
+
+
+public class myBrewGUITest {
+
+	public static void main(String[] args) throws FindFailed {
+		ImagePath.setBundlePath("./images/");
+		Screen s = new Screen();
+		Region r = new Region(0,0,100,700);
+		Match m = r.find("mainTab_selected.png");
+		System.out.print("Found at X: " + m.getX() + " Y: " + m.getY());
+		
+		//s.setAutoWaitTimeout(20.0);
+		//System.out.print("current timeout: " + s.getAutoWaitTimeout());
+		//assertNotNull("test exist", s.exists(new Pattern("cooler.png").similar(0.2f)));
+		//s.click(new Pattern("cooler.png").similar(0.2f));
+		//Pattern maintab = new Pattern("mainTab_selected.png").similar(0.5f);
+		//assertNotNull("Verify correct tab at start", s.exists(maintab));
+		//s.click(new Pattern("calc6.png").similar(0.5f));
+		//s.click(maintab);
+		//s.click("settingsTab_deselected.png");
+		//assertNotNull("Verify correct tab", s.exists("mainTab_deselected.png"));
+		//assertNotNull("Verify correct tab", s.exists("settingsTab_selected.png"));
+
+	}
+
+}
