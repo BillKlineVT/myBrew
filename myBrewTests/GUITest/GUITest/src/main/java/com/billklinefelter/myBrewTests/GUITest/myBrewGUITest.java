58ab17f1d5897d1c8a21d7e06c281fa2f171cbb8
Bill Klinefelter <bill.klinefelter@gmail.com>
2019-10-13 23:10:01 -0500

    fixed images for Fedora 30 test asset graphics

--- myBrewTests/GUITest/GUITest/src/main/java/com/billklinefelter/myBrewTests/GUITest/myBrewGUITest.java
+++ myBrewTests/GUITest/GUITest/src/main/java/com/billklinefelter/myBrewTests/GUITest/myBrewGUITest.java
@@ -9,21 +9,22 @@ public class myBrewGUITest {
 	public static void main(String[] args) throws FindFailed {
 		ImagePath.setBundlePath("./images/");
 		Screen s = new Screen();
-		Region r = new Region(0,0,100,700);
+		//Region r = new Region(0,0,100,700);
 		//Match m = r.find("mainTab_selected.png");
 		//System.out.print("Found at X: " + m.getX() + " Y: " + m.getY());
 		
 		//s.setAutoWaitTimeout(20.0);
 		System.out.print("current timeout: " + s.getAutoWaitTimeout());
-		assertNotNull("test exist", s.exists(new Pattern("cooler.png").similar(0.2f)));
-		s.click(new Pattern("cooler.png").similar(0.2f));
-		//Pattern maintab = new Pattern("mainTab_selected.png").similar(0.5f);
-		//assertNotNull("Verify correct tab at start", s.exists(maintab));
-		//s.click(new Pattern("calc6.png").similar(0.5f));
-		//s.click(maintab);
-		//s.click("settingsTab_deselected.png");
-		//assertNotNull("Verify correct tab", s.exists("mainTab_deselected.png"));
-		//assertNotNull("Verify correct tab", s.exists("settingsTab_selected.png"));
+		assertNotNull("test exist", s.exists(new Pattern("cooler.png").similar(0.8f)));
+		s.click(new Pattern("cooler.png").similar(0.8f));
+		Pattern maintab = new Pattern("mainTab_selected.png").similar(0.8f);
+		assertNotNull("Verify correct tab at start", s.exists(maintab));
+		s.click(maintab);
+		s.click("settingsTab_deselected.png");
+		assertNotNull("Verify correct tab", s.exists("mainTab_deselected.png"));
+		assertNotNull("Verify correct tab", s.exists("settingsTab_selected.png"));
+
+		//Mouse.move(20,50);
 
 	}
 
