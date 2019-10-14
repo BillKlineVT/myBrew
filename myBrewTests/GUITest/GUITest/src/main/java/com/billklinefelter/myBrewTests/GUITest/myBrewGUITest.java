b5d1baf7c84f5a865792378fc968a7a3a11fed02
Bill Klinefelter <bill.klinefelter@gmail.com>
2019-10-13 23:46:24 -0500

    start of GUI Automation Framework

--- myBrewTests/GUITest/GUITest/src/main/java/com/billklinefelter/myBrewTests/GUITest/myBrewGUITest.java
+++ myBrewTests/GUITest/GUITest/src/main/java/com/billklinefelter/myBrewTests/GUITest/myBrewGUITest.java
@@ -6,25 +6,94 @@ import static org.junit.Assert.*;
 
 public class myBrewGUITest {
 
+	public static boolean clickTab(Screen screen_var, Pattern selected, Pattern deselected) {
+		if (screen_var.exists(selected) != null) {
+			System.out.print("Already on tab, nothing to do...");
+			return true;
+		}
+		else if (screen_var.exists(deselected) != null) {
+			try {
+				screen_var.click(deselected);
+			} catch (FindFailed e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean setState(Screen screen_var, String state) {
+		
+		double similarity = 0.8;
+		
+		switch (state) {
+		case "main":
+			Pattern mainTab_selected = new Pattern("mainTab_selected.png").similar(similarity);
+			Pattern mainTab_deselected = new Pattern("mainTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, mainTab_selected, mainTab_deselected);
+		case "settings":
+			Pattern settingsTab_selected = new Pattern("settingsTab_selected.png").similar(similarity);
+			Pattern settingsTab_deselected = new Pattern("settingsTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, settingsTab_selected, settingsTab_deselected);
+		case "recipe":
+			Pattern recipeTab_selected = new Pattern("recipeTab_selected.png").similar(similarity);
+			Pattern recipeTab_deselected = new Pattern("recipeTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, recipeTab_selected, recipeTab_deselected);
+		case "hlt":
+			Pattern hltTab_selected = new Pattern("hltTab_selected.png").similar(similarity);
+			Pattern hltTab_deselected = new Pattern("hltTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, hltTab_selected, hltTab_deselected);
+		case "mlt":
+			Pattern mltTab_selected = new Pattern("mltTab_selected.png").similar(similarity);
+			Pattern mltTab_deselected = new Pattern("mltTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, mltTab_selected, mltTab_deselected);
+		case "boil":
+			Pattern boilTab_selected = new Pattern("boilTab_selected.png").similar(similarity);
+			Pattern boilTab_deselected = new Pattern("boilTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, boilTab_selected, boilTab_deselected);
+		case "chill":
+			Pattern chillTab_selected = new Pattern("chillTab_selected.png").similar(similarity);
+			Pattern chillTab_deselected = new Pattern("chillTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, chillTab_selected, chillTab_deselected);
+		case "clean":
+			Pattern cleanTab_selected = new Pattern("cleanTab_selected.png").similar(similarity);
+			Pattern cleanTab_deselected = new Pattern("cleanTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, cleanTab_selected, cleanTab_deselected);
+		case "timers":
+			Pattern timerTab_selected = new Pattern("timerTab_selected.png").similar(similarity);
+			Pattern timerTab_deselected = new Pattern("timerTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, timerTab_selected, timerTab_deselected);
+		case "logs":
+			Pattern logsTab_selected = new Pattern("logsTab_selected.png").similar(similarity);
+			Pattern logsTab_deselected = new Pattern("logsTab_deselected.png").similar(similarity);
+			return clickTab(screen_var, logsTab_selected, logsTab_deselected);
+		case "checklist":
+			Pattern checklistTab_selected = new Pattern("checklistTab_selected.png").similar(similarity);
+			Pattern checklistTab_deselected = new Pattern("checklistTab_deselected.png").similar(similarity);
+			boolean result = clickTab(screen_var, checklistTab_selected, checklistTab_deselected);
+			return result;
+		default:
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) throws FindFailed {
 		ImagePath.setBundlePath("./images/");
 		Screen s = new Screen();
-		//Region r = new Region(0,0,100,700);
-		//Match m = r.find("mainTab_selected.png");
-		//System.out.print("Found at X: " + m.getX() + " Y: " + m.getY());
 		
-		//s.setAutoWaitTimeout(20.0);
-		System.out.print("current timeout: " + s.getAutoWaitTimeout());
-		assertNotNull("test exist", s.exists(new Pattern("cooler.png").similar(0.8f)));
-		s.click(new Pattern("cooler.png").similar(0.8f));
-		Pattern maintab = new Pattern("mainTab_selected.png").similar(0.8f);
-		assertNotNull("Verify correct tab at start", s.exists(maintab));
-		s.click(maintab);
-		s.click("settingsTab_deselected.png");
-		assertNotNull("Verify correct tab", s.exists("mainTab_deselected.png"));
-		assertNotNull("Verify correct tab", s.exists("settingsTab_selected.png"));
-
-		//Mouse.move(20,50);
+		setState(s, "main");
+		setState(s, "settings");
+		setState(s, "recipe");
+		setState(s, "hlt");
+		setState(s, "mlt");
+		setState(s, "boil");
+		setState(s, "chill");
+		setState(s, "clean");
+		setState(s, "timers");
+		setState(s, "logs");
+		setState(s, "checklist");
 
 	}
 
